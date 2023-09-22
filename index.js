899f1c34bbb036ba08b62cc674cd39bebcbd082b
Abdullah Ahmad <abdullahaak06@gmail.com>
2023-09-22 19:47:59 +0500

    attempt 2 at fixing deployment issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-const Person = require('./models/person')
+const Person = require('./models/person.js')
 
 
 app.use(express.json())
